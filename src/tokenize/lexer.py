19c2c807592110e2ebb31959226d7ddbbdc59fa7
Willi <rohrpostamt@googlemail.com>
2011-04-02 13:54:18 +0200

    wrote token generator, clarified class hierarchy, added WhitespaceToken, added CharacterToken for unknown chars, removed silly mistake

--- src/tokenize/lexer.py
+++ src/tokenize/lexer.py
@@ -7,16 +7,21 @@ class BaseToken(object):
     def __init__(self, value):
         self.value = value
 
-class IdentifierToken(object):
+class IdentifierToken(BaseToken):
     def __init__(self, name):
         self.name = name
 
-class OperatorToken(object):
-    def __init__(self, value):
-        self.value = value
+class OperatorToken(BaseToken):
+    pass
 
-def ValueToken(self, value): # chars later
-    self.value = value
+class ValueToken(BaseToken): # chars later
+    pass
+
+class WhiteSpaceToken(BaseToken):
+    pass
+
+class CharacterToken(BaseToken):
+    pass
 
 # Numbers are floats and ints
 NUMBER = re.compile('\d+(?:\.\d+)?')
@@ -24,6 +29,7 @@ NUMBER = re.compile('\d+(?:\.\d+)?')
 # Can start letters and _
 # at least one letter or _ is required
 # ? is possible only at the end
+# ++ = #x:x+1. as builtin?
 IDENTIFIER = re.compile('[a-zA-Z_]+\w*[\?]{0,1}')
 
 # check at first ( and ) and then #, = and .
@@ -32,7 +38,42 @@ BASETOKEN = re.compile('\(|\)|#\|=|\.')
 # match without order
 OPERATORTOKEN = re.compile('&|\||\^|\+|\-|/|%|\*{1,2}|==|>=|==|<=|!=')
 
+COMMENT = re.compile(';.*') # not quite happy with comments after ;
 
 
-# note to myself: wenn whitespace, nächsten Durchlauf
-# Definition von ++ = #x:x+1. sollte auch gültig sein, als builtin?
+def tokenize(string):
+    while string:
+        if string.isspace():
+            yield WhiteSpaceToken()
+            string = string[1:]
+
+        comment = COMMENT.match(string).group(0)
+        identifier = IDENTIFIER.match(string).group(0)
+        number = NUMBER.match(string).group(0)
+        baseToken = BASETOKEN.match(string).group(0)
+        operatorToken = OPERATORTOKEN.match(string).group(0)
+
+        if comment:
+            string = string[len(comment):]   # skip comments
+
+        elif identifier:
+            yield IdentifierToken(identifier)
+            string = string[len(identifier):]
+
+        elif number:
+            yield NumberToken(number)
+            string = string[len(number):]
+
+        elif baseToken:
+            yield BaseToken(baseToken)
+            string = string[len(baseToken):]
+
+        elif operatorToken:
+            yield OperatorToken(operatorToken)
+            string = string[len(operatorToken):]
+
+        else:
+            yield CharacterToken(string[0])
+            string = string[1:]
+
+    yield EOFToken()
