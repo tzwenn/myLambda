b5f4f7674aff6abdbfa8964be177eacbb3866b13
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-04-03 01:33:54 +0200

    Builtins operate on symbols and return them, but internally they surely need python's numeric values

--- src/builtins.py
+++ src/builtins.py
@@ -2,6 +2,7 @@
 #	Decorator possible?
 
 import shareds
+from symbols import Value
 
 class BuiltIns(object):
 
@@ -10,14 +11,18 @@ class BuiltIns(object):
 			raise TypeError, "Expected callable for evaluate"
 		self.__ev = evaluate
 		self.__builtins = {
-			'+': lambda a, b: self.__ev(a) + self.__ev(b),
-			'-': lambda a, b: self.__ev(a) - self.__ev(b),
-			'*': lambda a, b: self.__ev(a) * self.__ev(b),
-			'/': lambda a, b: self.__ev(a) / self.__ev(b),
-			'%': lambda a, b: self.__ev(a) % self.__ev(b),
-			'**': lambda a, b: self.__ev(a) ** self.__ev(b),
+			'+': lambda a, b: Value(self.__syToNmb(a) + self.__syToNmb(b)),
+			'-': lambda a, b: Value(self.__syToNmb(a) - self.__syToNmb(b)),
+			'*': lambda a, b: Value(self.__syToNmb(a) * self.__syToNmb(b)),
+			'/': lambda a, b: Value(self.__syToNmb(a) / self.__syToNmb(b)),
+			'%': lambda a, b: Value(self.__syToNmb(a) % self.__syToNmb(b)),
+			'**': lambda a, b: Value(self.__syToNmb(a) ** self.__syToNmb(b)),
 			'if': lambda cond, yes, no: self.__ev(yes if toBool(self.__ev(cond)) else no),
 		}
+
+	def __syToNmb(self, symbol):
+		"""Symbol to numeric value"""
+		return self.__ev(symbol).value
 
 
 	def doIt(self, key, *args):
