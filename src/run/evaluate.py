30169dde3bac832c756662d63fddd08d1ba7378c
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-04-03 00:40:37 +0200

    NotImplemtedError for guess what: Not Implemented Symbols

--- src/run/evaluate.py
+++ src/run/evaluate.py
@@ -2,11 +2,19 @@ import symbols
 from builtins import BuiltIns
 from run import identifiers
 
+"""
+Exception, raised if a not yet implemented symbol is evaluated.
+"""
+class NotImplementedError(IndexError):
+	pass
+
 class Evironment(object):
 
 	def __init__(self):
 		self.identifiers = indentifiers.IdentifiersList()
 		self.builtins = BuiltIns(self.evaluate)
 
-	def evaluate(self):
-		pass
+	def evaluate(self, symbol):
+		if not isinstance(symbol, symbols.Symbol):
+			raise ValueError, "Can only evaluate symbols."
+		raise NotImplementedError, "Not implemented symbol %s" % type(symbol).__name__
