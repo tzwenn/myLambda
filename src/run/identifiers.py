645625656b3294b4ec132647b5b194a53e8b9093
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-04-02 11:49:07 +0200

    Docstrings, NameUnbound implements IndexError to abort for-loops

--- src/run/identifiers.py
+++ src/run/identifiers.py
@@ -4,14 +4,22 @@ Exception to be thrown if no more contexts can be poped.
 class NoContextError(ValueError):
 	pass
 
-
+"""
+Exception, raised if a name is already bound in the current context.
+"""
 class NameBoundError(NameError):
 	pass
 
-class NameUnboundError(NameError):
+"""
+Exception, raised if there is no identifier of that name in the current context.
+"""
+class NameUnboundError(IndexError):
 	pass
 
 class IdentifiersList(object):
 
 	def __init__(self):
 		self.__idents = [{}]
+
+	def __getitem__(self):
+		pass
