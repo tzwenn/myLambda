2c35367a8251be519b5c3ac663dc2a6d9f63bacd
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-04-12 22:25:58 +0200

    Console Skeleton

--- src/console.py
+++ src/console.py
@@ -5,6 +5,20 @@ class LambdaConsole(cmd.Cmd):
 	def __init__(self):
 		cmd.Cmd.__init__(self)
 		self.prompt = "-: "
+		self.intro = "myLambda (C) 2011 Lolcathorst\n" \
+		             "Type \"help\" for more information."
 
-	def do_exit(self, prm):
+	def emptyline(self):
+		pass # Empty lines have no effect
+
+	def default(self, line):
+		pass # TODO: Let there be action!
+
+	def help_quit(self):
+		print "Quits the interactive console"
+
+	def help_help(self):
+		print "Type help <command> for help about <command>"
+
+	def do_quit(self, prm):
 		return True
--- src/mylambda.py
+++ src/mylambda.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
-	print "Hello World"
+	from console import LambdaConsole
+	cmd = LambdaConsole()
+	cmd.cmdloop()
