0e6977edf3172e2d5e76baf50e6248e171aa9ac8
Willi <willi.mueller@student.hpi.uni-potsdam.de>
2011-04-14 23:13:47 +0200

    added much saner parser skeleton

--- src/parse/parser.py
+++ src/parse/parser.py
@@ -7,83 +7,60 @@ import src.symbols as symbols
 
 
 class Parser(object):
-	"""Parses a given statement"""
+	"""Parses a given statement represented as token objects
+    """
 
-	def __init__(self, stream):
-		self.stream = stream
+	def __init__(self, tokens):
+		self.tokens = tokens
 
 	def parseCall(self, exp):
 		pass
 
-	def parseName(self, stream):
+	def parseName(self, tokens):
 		"""bind or cex: first is a name, determine next token to decide"""
-		for i in xrange(0, len(token)):
+		#for i in xrange(0, len(token)):
 			#skip whitespace?
-			pass
+		pass
 
 	def parse(self):
+		"""Main method which builds the parse tree
+		"""
+		#list(t for t in self.tokens)
+		#for i in xrange(0, len(stream)):
 
-		for i in xrange(0, len(stream)):
-
+		for t in self.tokens:
 			# current token could be begin of a binding or call expression
-			if isinstance(self.stream[i], symbols.Name):
-				parseName(self.stream[i:])
+			if isinstance(t, symbols.Name):
+				self.parseName()
 
 			# current token is a lambda expression
-			if isinstance(self.stream[i], symbols.BaseToken) and self.stream[i] == '#':
-				parseFunc(self.stream[i:])
+			if isinstance(t, symbols.BaseToken) and str(t) == '#':
+				self.parseFunc()
 
 			# current token is a function call
-			if isinstance(self.stream[i], symbols.Name) and self.stream[i+1] == '(':
-				parseCall(self.stream[i:]) # skipt witespace inside
+			if isinstance(t, symbols.Name) and self.stream[i+1] == '(':
+				self.parseCall()
 
 			# current token is a value
-			if isinstance(self.stream[i], symbols.Value):
-				parseValue(self.stream[i:])
+			if isinstance(t, symbols.Value):
+				self.parseValue()
 
 
 def createStatement(tokens):
-	"""create current stream of tokens
-	stream is expression + '.'
+	"""takes the token generator object and creates one stream of tokens 
+    which represent one statement without trailing '.'
 	"""
 	currentStream = []
-	for i in xrange(0, tokens):
+	for t in tokens:
 		current = tokens.next()
 		while str(current) != '.':	# '.' marks end of statement
 			currentStream.append(current)
-	return currentStream	# without trailing dot
+			current = tokens.next()
+		yield currentStream
 
-def parserGen(stream):
-	"""Returns a generator parser instances for each expression
+def parserGenerator(string):
+	"""Generator to create parser instances for each expression
 	"""
-	while not isinstance(current, EOFToken):
-		p = Parser(createStatement(lexer.tokenize(stream)))
-		yield p
-
-def driver():
-	"""Is interface to for the repl to create fancy parser instances that behave like one object to the outside world
-	"""
-
-"""Idea: generate a parser instance for each expression an parse it in parallel
-or create one parser instance which is able to parse expressions in parallel
-first idea might be better because state is better encapsulated
-"""
-
-
-
-
-"""
-Lexer Output:
-
-# <class 'lexer.BaseToken'>
-x <class 'symbols.Name'>
-: <class 'lexer.CharacterToken'>
-  <class 'lexer.WhiteSpaceToken'>
-+ <class 'symbols.Operator'>
-( <class 'lexer.BaseToken'>
-x <class 'symbols.Name'>
-  <class 'lexer.WhiteSpaceToken'>
-1.0 <class 'symbols.Value'>
-) <class 'lexer.BaseToken'>
-. <class 'lexer.BaseToken'>
-"""
+	statements = createStatement(lexer.tokenize(string))
+	for s in statements:
+		yield Parser(s) 	# call my return value with self.parse()
