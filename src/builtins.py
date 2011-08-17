86c757e9fe3cd0d5fdc9256254052a2e36be9970
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-08-17 18:10:06 +0200

    Categorized BuiltIns.funcs

--- src/builtins.py
+++ src/builtins.py
@@ -19,30 +19,37 @@ class BuiltIns(object):
 			raise TypeError, "Expected callable for evaluate"
 		self.__ev = evaluate
 		self.funcs = {
+			# Arithmetic
 			'+': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) + self.__syToNmb(b))),
 			'-': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) - self.__syToNmb(b))),
 			'*': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) * self.__syToNmb(b))),
 			'/': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) / self.__syToNmb(b))),
 			'%': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) % self.__syToNmb(b))),
 			'**': BuiltIn(2, lambda a, b: Value(self.__syToNmb(a) ** self.__syToNmb(b))),
+
+			# Comparison
 			'==': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) == self.__syToNmb(b))),
 			'<=': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) <= self.__syToNmb(b))),
 			'>=': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) >= self.__syToNmb(b))),
 			'<': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) < self.__syToNmb(b))),
 			'>': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) > self.__syToNmb(b))),
 			'!=': BuiltIn(2, lambda a, b:  Value(self.__syToNmb(a) != self.__syToNmb(b))),
+
+			# Boolean algebra
 			'&': BuiltIn(2, lambda a, b:  Value(self.__syToBol(a) and self.__syToBol(b))),
 			'|': BuiltIn(2, lambda a, b:  Value(self.__syToBol(a) or self.__syToBol(b))),
 			'^': BuiltIn(2, lambda a, b:  Value(self.__syToBol(a) ^ self.__syToBol(b))),
 			'!': BuiltIn(1, lambda a: Value(not self.__syToBol(a))),
 
-			'print': BuiltIn(1, lambda a: (lambda val: stdout.write("%s\n" % str(val)) or val)(self.__ev(a))),
+			# Input/Output
 			'input': BuiltIn(0, lambda: Value(float(raw_input()))),
+			'print': BuiltIn(1, lambda a: (lambda val: stdout.write("%s\n" % str(val)) or val)(self.__ev(a))),
 
-			#TODO: What about a search path?
-			'import': BuiltIn(1, lambda s: runfile(s.name+FileExt, self.__ev)),
-
+			# Conditional evaluation 
 			'if': BuiltIn(3, lambda cond, yes, no: self.__ev(yes if self.__syToBol(cond) else no)),
+
+			# Reading libs TODO: What about a search path?
+			'import': BuiltIn(1, lambda s: runfile(s.name+FileExt, self.__ev)),
 		}
 
 	def __syToNmb(self, symbol):
