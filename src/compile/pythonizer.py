878b05b6a25bd227105abd86a1c150e7eb5dc797
tzwenn <sven.koehler@student.hpi.uni-potsdam.de>
2011-09-09 15:53:54 +0200

    Compile me func-defs and returnables

--- src/compile/pythonizer.py
+++ src/compile/pythonizer.py
@@ -1,9 +1,15 @@
-# Convert myLambda symbols to Python
+import symbols
 
 class Pythonzier:
+	""" Converts myLambda symbols to Python """
 
 	def __init__(self):
 		pass
 
-	def convert(self, symbol):
-		pass
+	def __tr(self, symbol):
+		if isinstance(symbol, symbols.Func):
+			return "lambda %s: %s" % (", ".join(symbol.args), self.__tr(symbol.dfn))
+		elif isinstance(symbol, symbols.Returnable):
+			return str(symbol)
+		raise NotImplementedError, "Cannot compile symbol %s yet" % type(symbol).__name__
+
